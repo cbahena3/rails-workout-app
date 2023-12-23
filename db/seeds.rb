e7e49a4055208e9fd594b5904acf7197fc14977e
Cristian B <cris.bahena77@gmail.com>
2023-12-23 14:49:14 -0600

    added shoulder, arms, back workouts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# exercise = Exercise.create(name: "Lying Leg Curl", image:"https://fitnessprogramer.com/wp-content/uploads/2021/02/Leg-Curl.gif", description: "Leg curls primarily target the hamstrings, which consist of three muscles: the biceps femoris, semitendinosus, and semimembranosus.", sets: "4", reps: "8-12")
 
-# exercise = Exercise.create(name: "Dumbbell Bulgarian Split Squat", image:"https://fitnessprogramer.com/wp-content/uploads/2021/05/Dumbbell-Bulgarian-Split-Squat.gif", description: "The quadriceps, hamstrings, glutes, and stabilizer muscles all work together to perform the movement, resulting in increased strength and muscular balance.", sets: "4", reps: "8-12")
-
-# exercise = Exercise.create(name: "Deadlift", image:"https://fitnessprogramer.com/wp-content/uploads/2021/02/Barbell-Deadlift.gif", description: "Deadlifts primarily target the muscles of the lower body, including the hamstrings, glutes, and quadriceps, making them an excellent way to build strength and size in these muscle groups.", sets: "4", reps: "6-8")
-
-# exercise = Exercise.create(name: "Weighted Calf Raise", image:"https://fitnessprogramer.com/wp-content/uploads/2021/02/Dumbbell-Calf-Raise.gif", description: "The target muscle group of this exercise is designed to isolate the calf muscles, it can maximize muscle development by focusing more on the muscles in this area.", sets: "4", reps: "12")
+# exercise = Exercise.create(name: "", image:"", description: "", sets: "4", reps: "8-12")
